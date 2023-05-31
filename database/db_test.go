137b9a06d87404205d160ea37b0cc46b6fa83bfb
calgor <ahomayoni684@gmail.com>
2023-05-31 17:15:41 +0330

    added TestGetUserByUsername

--- database/db_test.go
+++ database/db_test.go
@@ -17,3 +17,19 @@ func TestAddUser(t *testing.T) {
 		t.Errorf("AddUser failed: %v", err)
 	}
 }
+
+// TestGetUserByUsername tests the GetUserByUsername function
+func TestGetUserByUsername(t *testing.T) {
+	user := &model.User{
+		Username: "testuser",
+		Password: "testpassword",
+	}
+	u, err := GetUserByUsername(user)
+	if err != nil {
+		t.Errorf("GetUserByUsername failed: %v", err)
+	}
+	// Verify that the retrieved user matches the expected user
+	if u.Username != user.Username || u.Password != user.Password {
+		t.Errorf("GetUserByUsername returned incorrect user")
+	}
+}
