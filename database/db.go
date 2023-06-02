f4705977bbf00007fefc7df72be91cea3b3632f6
calgor <ahomayoni684@gmail.com>
2023-06-02 11:18:52 +0330

    Run migrations under grave

--- database/db.go
+++ database/db.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"log"
-	"strconv"
 	"time"
 
 	model "github.com/Calgorr/Full-URL_Shortener/model"
@@ -25,57 +23,6 @@ var (
 	db  *sql.DB
 	err error
 )
-
-func RunMigrations() error {
-	db, err := sql.Open("postgres", "postgres://"+user+":"+password+"@"+host+":"+strconv.Itoa(port)+"/"+"postgres"+"?sslmode=disable")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	query := `
-	CREATE DATABASE url_shortener;
-
-	\\c url_shortener;
-
-	CREATE TABLE IF NOT EXISTS users (
-		userid SERIAL PRIMARY KEY,
-		created_at DATE,
-		username VARCHAR(255) UNIQUE,
-		password VARCHAR(255)
-	);
-
-	CREATE TABLE IF NOT EXISTS url (
-		ID SERIAL PRIMARY KEY,
-		UserID INT,
-		Longurl VARCHAR(255),
-		shorturl VARCHAR(255) UNIQUE,
-		used_times INT,
-		created_at TIMESTAMP,
-		last_used_at TIMESTAMP,
-		FOREIGN KEY (UserID) REFERENCES users(userid)
-	);
-
-	CREATE OR REPLACE FUNCTION delete_expired_url() RETURNS TRIGGER AS $$
-	BEGIN
-		DELETE FROM url
-		WHERE last_used_at <= NOW() - INTERVAL '1 day';
-
-		RETURN NULL;
-	END;
-	$$ LANGUAGE plpgsql;
-
-	CREATE TRIGGER trg_delete_expired_url
-	AFTER INSERT OR UPDATE OR DELETE ON url
-	FOR EACH ROW EXECUTE FUNCTION delete_expired_url();
-	`
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	return nil
-}
 
 // Connect establishes a Connection to the database
 func Connect() (*sql.DB, error) {
